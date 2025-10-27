846a37e31cb09866e1de4c3b5c20a7b74f3ecdcd
Cassio <cassioasrosa@gmail.com>
2025-10-27 16:53:29 -0300

    add rota principal adicionar aluno

--- main.py
+++ main.py
@@ -26,3 +26,16 @@ def home(request: Request):
         "index.html",
         {"request": request, "listar_alunos": alunos}
     )
+
+#Tela de cadastro
+@app.get("/cadastro", response_class=HTMLResponse)
+def tela_cadastro(request: Request):
+    return templates.TemplateResponse(
+        "cadastro.html",
+        {"request": request}
+    )
+
+@app.post("/cadastro")
+def salvar_aluno(nome:str = Form(...), nota: float = Form(...)):
+    alunos.append({"nome": nome, "nota": nota})
+    return RedirectResponse(url="/", status_code=303)
